83ccd638331710f1d7e9d9f019528435959826cb
Aylwin-22 <82055270+Aylwin-22@users.noreply.github.com>
2021-04-08 19:56:44 +0200

    copz gather
    copia gather

---
+++ muebles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="muebles" tilewidth="32" tileheight="32" tilecount="814" columns="22">
+ <image source="muebles.png" width="718" height="1214"/>
+</tileset>
